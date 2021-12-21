b540a81d14808aeadd3d1dabb32b09100a1e1c68
ARM22043813 <peerawit.boo@dome.tu.ac.th>
2021-12-21 23:24:41 +0700

    cratre403

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -6,7 +6,7 @@ const verifyToken = (req, res, next) => {
   const token = req.body.token || req.headers["x-access-token"];
 
   if (!token) {
-    return res.status(400).send("A token is required for authentication");
+    return res.status(403).send("A token is required for authentication");
   }
 
   try {
